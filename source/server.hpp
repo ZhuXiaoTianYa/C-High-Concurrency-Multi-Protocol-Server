fd8336b6e67584f168f0a190e6a5dc236da0fb59
zhuxiaotianya <1119876462@qq.com>
2026-03-30 23:48:16 +0800

    添加了Socket模块

--- source/server.hpp
+++ source/server.hpp
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <vector>
 #include <stdint.h>
 #include <cassert>
@@ -6,6 +7,10 @@
 #include <iostream>
 #include <ctime>
 #include <cstdarg>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #define DBG 0
 #define INF 1
@@ -25,10 +30,10 @@
         fprintf(stdout, "[%s %s:%d] " format "\n", timer, __FILE__, __LINE__, ##__VA_ARGS__); \
     } while (0)
 
-#define DBG_LOG(format, ...) LOG(DBG, format, __VA_ARGS__)
-#define INF_LOG(format, ...) LOG(INF, format, __VA_ARGS__)
-#define ERR_LOG(format, ...) LOG(ERR, format, __VA_ARGS__)
-#define FAT_LOG(format, ...) LOG(FAT, format, __VA_ARGS__)
+#define DBG_LOG(format, ...) LOG(DBG, format, ##__VA_ARGS__)
+#define INF_LOG(format, ...) LOG(INF, format, ##__VA_ARGS__)
+#define ERR_LOG(format, ...) LOG(ERR, format, ##__VA_ARGS__)
+#define FAT_LOG(format, ...) LOG(FAT, format, ##__VA_ARGS__)
 
 #define BUFFER_DEFAULT_SIZE 1024
 class Buffer
@@ -157,5 +162,169 @@ public:
     {
         _write_idx = 0;
         _read_idx = 0;
+    }
+};
+
+#define MAX_LISTEN 1024
+
+class Socket
+{
+private:
+    int _sockfd;
+
+public:
+    Socket() : _sockfd(-1) {}
+    Socket(const int &sockfd) : _sockfd(sockfd) {}
+    ~Socket() { Close(); }
+    int Fd() { return _sockfd; }
+    bool Create()
+    {
+        int newfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (newfd < 0)
+        {
+            ERR_LOG("CREATE SOCKET FAILED!");
+            return false;
+        }
+        _sockfd = newfd;
+        return true;
+    }
+    bool Bind(const std::string &ip, const uint16_t &port)
+    {
+        sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr(ip.c_str());
+        addr.sin_port = htons(port);
+        socklen_t len = sizeof(addr);
+        int ret = bind(_sockfd, (sockaddr *)&addr, len);
+        if (ret < 0)
+        {
+            ERR_LOG("BIND SOCKET FAILED!");
+            return false;
+        }
+        return true;
+    }
+
+    bool Listen(const int &backlog = MAX_LISTEN)
+    {
+        // 相当于内核标记这个套接字为监听套接字
+        // 内核的工作模式就变成了：
+        // 内核自动接管三次握手
+        // 自动维护一个半连接SYN队列和一个全连接Accept队列
+        // 这个时候内核就开始收SYN
+        // 回SYN+ACK
+        // 等最后的ACK
+
+        int ret = listen(_sockfd, backlog);
+        if (ret == -1)
+        {
+            FAT_LOG("LISTEN SOCKET FAILED!");
+            return false;
+        }
+        return true;
+    }
+
+    bool Connect(const std::string &ip, const uint16_t &port)
+    {
+        struct sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr(ip.c_str());
+        addr.sin_port = htons(port);
+        socklen_t len = sizeof(addr);
+        int ret = connect(_sockfd, (struct sockaddr *)&addr, len);
+        if (ret < 0)
+        {
+            ERR_LOG("CONNECT FAILED!");
+            return false;
+        }
+        return true;
+    }
+
+    int Accept()
+    {
+        // 相当于从全连接队列中拿出一个已经完成握手的客户端，然后创建一个全新的套接字给他
+        int ret = accept(_sockfd, NULL, NULL);
+        if (ret == -1)
+        {
+            ERR_LOG("ACCEPT SOCKET FAILED!");
+            return ret;
+        }
+        return ret;
+    }
+    ssize_t Recv(void *buf, const size_t &len, const int &flag = 0)
+    {
+        ssize_t ret = recv(_sockfd, buf, len, flag);
+        if (ret <= 0)
+        {
+            if (ret == EAGAIN || ret == EINTR)
+            {
+                return 0;
+            }
+            return -1;
+        }
+        return ret;
+    }
+
+    ssize_t NonBlockRecv(void *buf, const size_t &len)
+    {
+        return Recv(buf, len, MSG_DONTWAIT);
+    }
+    ssize_t Send(const void *buf, const size_t &len, const int &flag = 0)
+    {
+        ssize_t ret = send(_sockfd, buf, len, flag);
+        if (ret <= 0)
+        {
+            if (ret == EAGAIN || ret == EINTR)
+            {
+                return 0;
+            }
+            return -1;
+        }
+        return ret;
+    }
+
+    ssize_t NonBlockSend(void *buf, const size_t &len)
+    {
+        return Send(buf, len, MSG_DONTWAIT);
+    }
+    void Close()
+    {
+        if (_sockfd != -1)
+        {
+            close(_sockfd);
+            _sockfd = -1;
+        }
+    }
+    bool CreateServer(const uint16_t &port, const std::string &ip = "0.0.0.0", bool block_flag = true)
+    {
+        if (Create() == false)
+            return false;
+        if (Bind(ip, port) == false)
+            return false;
+        if (Listen() == false)
+            return false;
+        if (block_flag == false)
+            NonBlock();
+        ReuseAddress();
+        return true;
+    }
+    bool CreateClient(const uint16_t &port, const std::string &ip)
+    {
+        if (Create() == false)
+            return false;
+        if (Connect(ip, port) == false)
+            return false;
+        return true;
+    }
+    void ReuseAddress()
+    {
+        int val = 1;
+        setsockopt(_sockfd, SOL_SOCKET, SO_REUSEADDR, (void *)&val, sizeof(int));
+        val = 1;
+        setsockopt(_sockfd, SOL_SOCKET, SO_REUSEPORT, (void *)&val, sizeof(int));
+    }
+    void NonBlock()
+    {
+        int flag = fcntl(_sockfd, F_GETFD, 0);
+        fcntl(_sockfd, F_SETFD, flag | O_NONBLOCK);
     }
 };
